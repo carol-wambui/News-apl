3cffedc74e9cb86b3318e1ca7ce39e1653e26a9f
carol-wambui <carolwambuidaystar@gmail.com>
2020-11-29 15:42:18 +0300

    create news.html

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/news/<int:news_id>')
+def news(news_id):
+
+    '''
+    View news page function that returns the index page and its data
+    '''
+    return render_template('news.html',id = news_id)
+ 
+ 
